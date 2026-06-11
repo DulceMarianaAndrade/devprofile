aefc973bea2dfeca64ee1491ef0e7542f84961ff
Elia Arteaga Delgado <delgadolia76@gmail.com>
2026-06-11 15:45:17 -0600

    arreglo rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; 
 import { CVProvider } from "./context/CVContext";
 import App from "./App.jsx";
 import "./styles/global.css";
@@ -12,10 +12,10 @@ import "./styles/ThemeToggle.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CVProvider>
         <App />
       </CVProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
